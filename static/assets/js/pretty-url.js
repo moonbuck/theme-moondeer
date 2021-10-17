f02c9b2743f3529ad7f10e065e1723f944f94f58
Jason Cardwell <moonbuck@users.noreply.github.com>
2021-10-17 16:47:38 -0700

    Reverting pretty url code

--- static/assets/js/pretty-url.js
+++ static/assets/js/pretty-url.js
@@ -1,23 +1,7 @@
+/* Script for altering the URL to remove "/categories", requires the aliases configured in front matter under /content/categories/some-category/_index.md */
 var href = window.location.href;
 
-var href_sans_categories = href;
-
-if (href.includes("/categories") 
-&& !(href.endsWith("categories") || href.endsWith("categories/")) 
-{
-  href_sans_categories = href.replace("/categories", "");
-  
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function() {
-    
-  if (this.status < 400 && this.status >= 300) {
-      alert('this redirects to ' + this.getResponseHeader("Location"));
-    } else {
-      alert('doesn\'t redirect ');
-    }
-  }
-  xhr.open('HEAD', href_sans_categories, true);
-  xhr.send();
-
-     
+if (href.includes("/categories")) {
+  href = href.replace("/categories", "");
+  history.pushState({}, "", href);   
 }
