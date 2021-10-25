c62d67670c653fec712713f08700cf3396da2464
Jason Cardwell <moonbuck@users.noreply.github.com>
2021-10-25 10:21:48 -0700

    Forgot to delete a script

--- static/assets/js/glightbox.js
+++
@@ -1,9 +0,0 @@
-/* Unmodified code from the glightbox plugin */
-const lightbox = GLightbox(	
-	{
-		touchNavigation: true,
-	    touchFollowAxis: true,
-        keyboardNavigation: true,
-        closeButton: true
-	}
-);
