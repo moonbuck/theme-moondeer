89ce5d0296b1e46228eea55af7c6a0cc5d60bcd0
Jason Cardwell <moonbuck@users.noreply.github.com>
2021-11-28 07:30:44 -0800

    Parameterizing cardify.js

--- assets/js/cardify.js
+++ assets/js/cardify.js
@@ -9,17 +9,66 @@ reading-time?    article:reading_time
 publish-date?    article:publish_date
 
 */
-document.addEventListener('DOMContentLoaded',() => {
-  // handle DOMContentLoaded event
-  document.querySelectorAll('a[href$="cardify"]').forEach(link => processLink(link));
-});
 
+// Fetch the parameterized flag values
+const queryCards = {{ site.Params.Theme.Cardify.QueryCards }};
+const queryString = 'cardify';
+const hostMatchCards = {{ site.Params.Theme.Cardify.HostMatchCards }};
+const hostname = "{{ (urls.Parse site.BaseURL).Host | lower }}";
+
+// Add the event listener for a loaded DOM
+document.addEventListener('DOMContentLoaded',() => {
+  
+  // Return if we aren't meant to be generating cards
+  if !(queryCards || hostMatchCards) { return; }
+  
+  // Check whether we're only processing query tagged links
+  else if (queryCards && !hostMatchCards) {
+    
+    document.querySelectorAll(`a[href$="${queryString}"]`)
+            .forEach(link => processLink(link));
+          
+  }
+  
+  // Fetch query tagged and host matched links
+  else {
+    
+    // Create a set to hold cardifiable links
+    let links = new Set();
+    
+    document.links.forEach(link => {
+      
+      // Create a URL object from the provided link
+      let url = new URL(link);
+      
+      // Check whether the query string is present
+      // or the host name is match
+      if (   url.search.includes(queryString)
+          || url.hostname.toLowerCase() == hostname) {
+        
+        // Add the link to the set of links
+        links.add(link)
+        
+      }
+      
+    }); // document.links.forEach
+    
+    // Process any links we've gathered
+    links.forEach(link => processLink(link));
+    
+  }
+  
+  
+}); // document.addEventListener
+
+// Fetches page text and feeds it to scrapePage(html, link)
 function processLink(link) {  
   fetch(link)
     .then(response => response.text())
     .then(html => scrapePage(html, link));
 }
 
+// Selectors used to match meta tags
 const urlSel = 'meta[property="og:url"],meta[name="twitter:url"]';
 const imgSel = 'meta[property="og:image"],meta[name="twitter:image"]';
 const titleSel = 'meta[property="og:title"],meta[name="twitter:title"]';
@@ -27,6 +76,10 @@ const descSel = 'meta[property="og:description"],meta[name="twitter:description"
 const readingTimeSel = 'meta[name="article:reading_time"]';
 const publishDateSel = 'meta[property="article:published_time"]';
 
+// Creates a DOM from the provided HTML text, 
+// queries for meta tags, and replaces the provided link
+// with a new div element containing child elements
+// displaying the information fetched from the meta tags.
 function scrapePage(html, link) {
     	// Convert the HTML string into a document object
   	var parser = new DOMParser();
